ded78fdcf7718862ba27ea7c001c879d1abf6c0f
MatheoFernando <jvieirafernando20@gmail.com>
2025-05-15 15:50:57 +0100

    feat:scroll main

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,11 +9,13 @@ export default function Layout({
   return (
     <div className="flex h-screen overflow-hidden max-w-7xl mx-auto ">
       <Sidebar />
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <header className="h-36 flex items-center px-6">
-          <Header/>
+     <div className="flex flex-col flex-1 max-h-screen">
+       
+        <header className="h-36 flex items-center px-6 shrink-0">
+          <Header />
         </header>
-        <main className="flex-1 overflow-auto ">
+
+        <main className="flex-1 ">
           {children}
         </main>
       </div>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,7 +6,6 @@ export default function Dashboard() {
     <div className="flex h-screen overflow-hidden">
      
         <main className="flex-1  p-6 ">
-   <div className=" h-36 rounded w-full"></div>
         </main>
       
     </div>
